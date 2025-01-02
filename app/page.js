a51fb7f42f1273609056e1796ed52a01c700ae56
maniegon <nathan.maniego@mail.utoronto.ca>
2025-01-02 00:11:13 -0500

    Updated projects

--- app/page.js
+++ app/page.js
@@ -8,6 +8,8 @@ import {AiFillTwitterCircle,
 
 import deved from "../public/image 1.png"
 import p1 from "../public/design.png"
+import p2 from "../public/code.png"
+import p3 from "../public/consulting.png"
 
 
 export default function Home() {
@@ -28,7 +30,7 @@ export default function Home() {
             <h2 className="text-5xl py-2 text-purple-600 font-burtons">Nathan Maniego</h2>
             <h3 className="text-2xl py-2">Computer Science Student</h3>
             <p className="text-base py-2 leading-8 text-gray-800 ">Second year computer science student at the University of Toronto.
-              Passionate about Software Engineering and making money! </p>
+              Passionate about Software Engineering and Machine Learning! </p>
           <div className="relative bg-gradient-to-b from-purple-500 rounded-full w-80 h-80 mx-auto mt-10 overflow-hidden">
             <Image src={deved} alt="nathanmaniego avatar" layout="fill" objectFit="cover"></Image>
           </div>
@@ -45,47 +47,48 @@ export default function Home() {
               I’m a computer science enthusiast with a passion for writing clean, efficient code and exploring emerging technologies. Always eager to learn and adapt, I enjoy diving into <span className="text-purple-500"> complex problems, optimizing algorithms, and contributing to collaborative software projects.</span> Whether I’m developing applications, experimenting with new programming languages, or refining my systems design skills, I bring curiosity, precision, and a growth mindset to everything I do.
               </p>
             </div>
-            <div className="lg:flex gap-10">
-              <div className="text-center shadow-lg p-10 rounded-xl my-10" >
+            <div className="lg:flex gap-20">
+              <div className="text-center shadow-lg p-10 rounded-xl my-10 flex-1" >
                 <div className="flex justify-center ">
-                <Image alt={"project1"}src={p1} width={100} height={100}></Image>
+                <Image alt={"project1"}src={p2} width={100} height={100}></Image>
                 </div>
                 
-                <h3 className="text-lg font-medium pt-8 pb-2">Project 1</h3>
+                <h3 className="text-lg font-medium pt-8 pb-2">Project Bleu</h3>
                 <p className="py-2">
-                  Whether I’m developing applications, experimenting with new programming languages, or refining my systems design skills, I bring curiosity, precision, and a growth mindset to everything I do.
+                  Developed and deployed a Random Forest machine learning model with advanced data visualization tools to optimize stormwater management through blue roof systems, achieving 80% retention and 
+                  earning an Honourable Mention Award for our innovative solution.
                 </p>
-                <h4 className="py-2 text-purple-800">Design tools I use</h4>
-                <p className="text-gray-800 py-1">Java</p>
-                <p className="text-gray-800 py-1">JavaFX</p>
-                <p className="text-gray-800 py-1">Ollama3</p>
+                <h4 className="py-2 text-purple-800">Code tools I used</h4>
+                <p className="text-gray-800 py-1">Python</p>
+                <p className="text-gray-800 py-1">Pandas</p>
+                <p className="text-gray-800 py-1">React JS</p>
               </div>
 
-              <div className="text-center shadow-lg p-10 rounded-xl my-10" >
+              <div className="text-center shadow-lg p-10 rounded-xl my-10 flex-1" >
                 <div className="flex justify-center ">
                 <Image alt={"project1"}src={p1} width={100} height={100}></Image>
                 </div>
                 
-                <h3 className="text-lg font-medium pt-8 pb-2">Project 1</h3>
+                <h3 className="text-lg font-medium pt-8 pb-2">S&P 500 Stock Predictor</h3>
                 <p className="py-2">
-                  Whether I’m developing applications, experimenting with new programming languages, or refining my systems design skills, I bring curiosity, precision, and a growth mindset to everything I do.
+                  Developed a Random Forest machine learning model to predict daily movements of the S&P 500 index, achieving a 54% precision score by integrating advanced features like multi-horizon rolling averages and trend ratios from decades of historical data.
                 </p>
-                <h4 className="py-2 text-purple-800">Design tools I use</h4>
-                <p className="text-gray-800 py-1">Java</p>
-                <p className="text-gray-800 py-1">JavaFX</p>
-                <p className="text-gray-800 py-1">Ollama3</p>
+                <h4 className="py-2 text-purple-800">Code tools I used</h4>
+                <p className="text-gray-800 py-1">Python</p>
+                <p className="text-gray-800 py-1">Pandas</p>
+                <p className="text-gray-800 py-1">Matplotlib</p>
               </div>
 
-              <div className="text-center shadow-lg p-10 rounded-xl my-10" >
+              <div className="text-center shadow-lg p-10 rounded-xl my-10 flex-1" >
                 <div className="flex justify-center ">
-                <Image alt={"project1"}src={p1} width={100} height={100}></Image>
+                <Image alt={"project1"}src={p3} width={100} height={100}></Image>
                 </div>
                 
-                <h3 className="text-lg font-medium pt-8 pb-2">Project 1</h3>
+                <h3 className="text-lg font-medium pt-8 pb-2">MS Paint Inspired Clone</h3>
                 <p className="py-2">
-                  Whether I’m developing applications, experimenting with new programming languages, or refining my systems design skills, I bring curiosity, precision, and a growth mindset to everything I do.
+                  Developed a Java-based paint application with tools for drawing, shape selection, and customizable color and size options, incorporating real-time feedback for enhanced user engagement. Integrated the Ollama API for seamless parseable file compatibility.
                 </p>
-                <h4 className="py-2 text-purple-800">Design tools I use</h4>
+                <h4 className="py-2 text-purple-800">Code tools I used</h4>
                 <p className="text-gray-800 py-1">Java</p>
                 <p className="text-gray-800 py-1">JavaFX</p>
                 <p className="text-gray-800 py-1">Ollama3</p>
